bae9b0aa8767b0ff844eaa7d1884925c8f37ff8a
DamodarE <114298784+DamodarE@users.noreply.github.com>
2022-10-17 18:41:11 -0400

    Compacted into one file

--- Wordle.java
+++ Wordle.java
@@ -1,9 +1,53 @@
 import java.util.Scanner;
-import com.javatpoint.wordFile;
+import java.net.*;  
+import java.io.*; 
 public class Wordle{
+
+    public static String check(String word)
+        {
+            // Determines if the word is the right length and is in the word list
+            Scanner scanner = new Scanner(System.in);
+            String wordList = Wordle.getUrlContents("https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt");
+            while(word.length() != 5 || !wordList.contains(word.toLowerCase()))
+            {
+                System.out.println("That is not a valid word. Try again1!");
+                word = scanner.nextLine().toUpperCase();
+            }
+            return word;
+            
+        }   
+
+    public static String getUrlContents(String theUrl)  
+    {   
+    StringBuilder content = new StringBuilder();  
+    // Use try and catch to avoid the exceptions  
+    try  
+    {  
+      URL url = new URL(theUrl); // creating a url object  
+      URLConnection urlConnection = url.openConnection(); // creating a urlconnection object  
+  
+      // wrapping the urlconnection in a bufferedreader  
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));  
+      String line;  
+      // reading from the urlconnection using the bufferedreader  
+      while ((line = bufferedReader.readLine()) != null)  
+      {  
+        content.append(line);  
+      }  
+      bufferedReader.close();  
+    }  
+    
+    catch(Exception e)  
+    {  
+      e.printStackTrace();  
+    }  
+    return content.toString();  
+  }  
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String wordList = wordFile.getUrlContents("https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt");
+        String wordList = Wordle.getUrlContents("https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt");
+
+        
 
         //Computer chooses the random word;
         int randomWordNumber = (int) (Math.random() * wordList.length());
@@ -12,19 +56,18 @@ public class Wordle{
             randomWordNumber = (int) (Math.random() * wordList.length());
         }
         String computerGuess = wordList.substring((randomWordNumber), (randomWordNumber) + 5).toUpperCase();
-        System.out.println(computerGuess);
 
         //Asking the user for a word input
         //Checking if the input is a valid word that is 5 characters long
         //Evaluating in each letter in the guess to see if it corresponds with the correct letter or is in the word at all
         //After 6 guesses, if the user still has not guessed the word, they lose
-        for(int tries = 5; tries >= 0; tries--){
-            System.out.println("Enter a five letter word: ");
+        for(int tries = 6; tries >= 0; tries--){
+            System.out.println("\nEnter a five letter word: ");
             String userGuess = scanner.nextLine().toUpperCase();
-            userGuess = checkWord.check(userGuess);
+            userGuess = Wordle.check(userGuess);
                 for(int i = 0; i < userGuess.length(); i++)
                 {
-                    if(computerGuess.contains(userGuess.substring(i, i+1))&& userGuess.indexOf(userGuess.substring(i, i+1)) == i)
+                    if(computerGuess.contains(userGuess.substring(i, i+1)))
                     {
                         if(userGuess.substring(i, i + 1).equals(computerGuess.substring(i, i + 1)))
                         {
